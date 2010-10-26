718ee807347c7eb7eed07cce105cc7dec42b531d
somename <someemail>
2010-10-26 16:41:34 -0200

    besouro automatic message

--- src/PrimeGenerator.java
+++ src/PrimeGenerator.java
@@ -11,22 +11,13 @@ public class PrimeGenerator {
 			primesList.add(n);
 		}
 		
-		for (int m=0; m<primesList.size() ; m++) {
-			for (int n=0; n<primesList.size() ; n++) {
-				int first = primesList.get(m);
-				int seccond = primesList.get(n);
-				System.out.println("removing " + first*seccond);
-				primesList.remove(new Integer(first*seccond));
-			}
-		}
 		
-//		
-//		for (int n=2; n*2<=i ; n++) {
-//			primesList.remove(new Integer(n*2));
-//		}
-//		for (int n=2; n*3<=i ; n++) {
-//			primesList.remove(new Integer(n*3));
-//		}
+		for (int n=2; n*2<=i ; n++) {
+			primesList.remove(new Integer(n*2));
+		}
+		for (int n=2; n*3<=i ; n++) {
+			primesList.remove(new Integer(n*3));
+		}
 		
 		return primesList.toArray(new Integer[primesList.size()]);
 		
