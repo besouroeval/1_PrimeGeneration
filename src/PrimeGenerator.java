12a55d28c17eb6918146276930c08e88826b11b7
somename <someemail>
2010-10-26 16:45:56 -0200

    besouro automatic message

--- src/PrimeGenerator.java
+++ src/PrimeGenerator.java
@@ -5,6 +5,8 @@ public class PrimeGenerator {
 
 	public static Integer[] generate(int i) {
 		
+		System.out.println("up to  " + i);
+		
 		ArrayList<Integer> primesList = new ArrayList<Integer>();
 		
 		for (int n=2; n<=i ; n++) {
