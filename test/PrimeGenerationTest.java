7df740496c78a07262f7347cde21d3193ecd9f66
somename <someemail>
2010-10-26 16:08:29 -0200

    besouro automatic message

--- test/PrimeGenerationTest.java
+++ test/PrimeGenerationTest.java
@@ -14,6 +14,7 @@ public class PrimeGenerationTest {
 	@Test
 	public void shouldGenerateUpTo2() {
 		int[] primes = PrimeGenerator.generate(2);
-		Assert.assertEquals(0, primes.length);
+		Assert.assertEquals(1, primes.length);
+		Assert.assertEquals(2, primes[0]);
 	}
 }
