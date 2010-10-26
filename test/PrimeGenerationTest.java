1b025138b2bc0300e660f030e30c118bd2bf228a
somename <someemail>
2010-10-26 16:24:04 -0200

    besouro automatic message

--- test/PrimeGenerationTest.java
+++ test/PrimeGenerationTest.java
@@ -15,6 +15,7 @@ public class PrimeGenerationTest {
 		verifyGeneration(6, new int[]{2,3,5});
 		verifyGeneration(7, new int[]{2,3,5,7});
 		verifyGeneration(8, new int[]{2,3,5,7});
+		verifyGeneration(9, new int[]{2,3,5,7});
 	}
 
 	private void verifyGeneration(int i, int[] expected) {
